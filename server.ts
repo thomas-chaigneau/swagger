c6af40f2b82a782526a6224a5a8af164fbbe7e65
Thomas CHAIGNEAU <t.chaigneau@groupeonepoint.com>
2021-03-25 19:42:32 +0100

    init commit

---
+++ server.ts
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = 3001;
+
+app.get('/', (req, res) => {
+  res.send('coucou!');
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
