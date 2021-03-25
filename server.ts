931cd39c804655ff8b9ad1dacdafe0f8c2d0ad12
Thomas CHAIGNEAU <t.chaigneau@groupeonepoint.com>
2021-03-25 19:50:45 +0100

    nodemon config

--- server.ts
+++ server.ts
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3001;
 
 app.get('/', (req, res) => {
-  res.send('coucou!');
+  res.send('coucou, nodemon ????!?????');
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
